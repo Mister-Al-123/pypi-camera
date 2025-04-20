0f6a44f82055be6a9fa809dc35e95a7ab814884b
Al <alzy@noreply.localhost>
2025-04-20 16:42:16 +0100

    Re-ordered files array to show most recent clips

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,28 +8,36 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="flex p-6 text-gray-900">
+                <div class="p-6 text-gray-900">
+                    <p class="text-4xl font-extrabold">
+                        Stream
+                    </p>
                     <div class="ml-2">
-                    <iframe src="/live/?controls=false" width="640" height="480"></iframe></div>
-                    <div class="ml-2">
-                            <div class="pb-2">
-                            </div>
-                            <div class="pb-2">
-                                <p><?php
-                                $dir = "/camera";
-                                $files = array_diff(scandir($dir), array('.', '..'));
-                                foreach ($files as $file) {
-                                        echo '
-                                        <video controls>
+                        <iframe src="/live/?controls=false" width="640" height="480"></iframe>
+                    </div>
+                    <div>
+                        <p class="text-4xl font-extrabold">
+                            Clips
+                        </p>
+                        <?php
+                            $dir = "/camera";
+                            $files = array_diff(scandir($dir), array('.', '..'));
+                            $files = array_reverse($files);
+                            foreach ($files as $file) {
+                                    $filename = "$file";
+                                    echo '
+                                    <div class="flex p-4 shadow-sm sm:rounded-lg bg-slate-300">
+                                        <video width="320" height="240" controls>
                                             <source src="'.$dir.'/'.$file.'" type="video/mp4">
-                                        </video>';
-                                }
-                                    
-                                    ?></p>
-                            </div>
-                            <div class="pb-2">
-                                <p>Test text</p>
-                            </div>
+                                        </video>
+                                        <p class="ml-2 text-2xl font-extrabold">
+                                            '.$filename.'
+                                        </p class="p-6 flex">
+                                        <p> Button </p>
+                                    </div>
+                                    ';
+                            }           
+                        ?>
                     </div>
                 </div>
                 <div class="ml-2 text-grey-900">
